dddd9417217bd4f3bf965a1f70849036e79949e1
chrisdews <cldews1@gmail.com>
2019-06-17 13:48:51 +0100

    User login and authentication unfinished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    # byebug
+    if !user || !user.authenticate(params[:password])
+      flash[:errors] = ["Credentials aren't valid"]
+      # redirect_to new_session_path
+    else
+      session[:user_id] = user.id
+      # redirect_to new_user_path
+      # not configured yet
+    end
+
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to login_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_090800) do
+ActiveRecord::Schema.define(version: 2019_06_17_123547) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_06_17_090800) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end

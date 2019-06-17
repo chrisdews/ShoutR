aa804a839874c514b709056328923e84fd74a447
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-06-17 10:29:46 +0100

    added data and view for shout

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_150056) do
+ActiveRecord::Schema.define(version: 2019_06_17_090800) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
     t.integer "shout_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["shout_id"], name: "index_comments_on_shout_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -38,7 +40,7 @@ ActiveRecord::Schema.define(version: 2019_06_14_150056) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "password"
     t.date "date_of_birth"
     t.string "email"

d15e74bbdb6afe8dae0fb9370d08ff2c43e8101c
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-06-17 11:14:50 +0100

    No 2

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,2 +1,31 @@
 class ShoutsController < ApplicationController
+  def index
+    @shouts = Shout.all
+  end
+
+  def show
+    @shout = Shout.find(params[:id])
+  end
+
+  def new
+    @shout = Shout.new
+  end
+
+  def create
+    @shout = Shout.new(shoutparams)
+    @shout.save
+    redirect_to shouts_path
+  end
+
+  def destroy
+    @shout = Shout.find(params[:id])
+    @shout.destroy
+    redirect_to "/shouts"
+  end
+
+  private
+
+  def shoutparams
+    params.require(:shout).permit(:text)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+20.times do
+  User.create(username: Faker::Internet.username(5..8), password: Faker::Internet.password(5, 10), email: Faker::Internet.email, date_of_birth: Faker::Date.birthday(18, 65))
+end
+
+50.times do
+  Shout.create(text: Faker::Quote.famous_last_words, user_id: rand(1..20))
+end
+
+80.times do
+  Comment.create(text: Faker::Quote.famous_last_words, shout_id: rand(1..50), user_id: rand(1..20))
+  Like.create(shout_id: rand(1..50), user_id: rand(1..20))
+end

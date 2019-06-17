e5247e64d719f1dadfb226e01f317bd5ca482efe
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-06-17 18:20:49 +0100

    added likes for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ApplicationRecord
   has_many :likes, as: :likeable
   belongs_to :shout
   belongs_to :user
+
+  def has_like?(user)
+    self.likes.find_by(user_id: user.id)
+  end
+
 end
